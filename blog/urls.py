0ea606889d540bdc51915104f586083e3250dd85
DanyloSamoylov <danylosamoylov@gmail.com>
2022-04-08 18:46:28 +0300

    forms and user accounts

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 # first we created this file with touch blog/urls.py
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView, BlogCreateNew, BlogUpdateView, BlogDeleteView
 
 urlpatterns = [
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
+    path('post/new/', BlogCreateNew.as_view(), name='post_new'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
     path('', BlogListView.as_view(), name='home'),
 ]
